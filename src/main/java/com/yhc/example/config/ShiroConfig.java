4288bfddc271b0329143e9186d5ebf2523077c9b
yanghaichuan <51939690+yanghaichuan@users.noreply.github.com>
2020-08-10 09:14:18 +0800

    layui

--- src/main/java/com/yhc/example/config/ShiroConfig.java
+++ src/main/java/com/yhc/example/config/ShiroConfig.java
@@ -71,7 +71,7 @@ public class ShiroConfig {
         // 配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了, 位置放在 anon、authc下面
         filterChainDefinitionMap.put("/logout", "logout");
 
-        // 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
+        // 如果不设置默认会自动寻找Web工程根目录下的"/system.jsp"页面
         // 配器shirot认登录累面地址，前后端分离中登录累面跳转应由前端路由控制，后台仅返回json数据, 对应LoginController中unauth请求
         shiroFilterFactoryBean.setLoginUrl("/");
 
